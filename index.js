2ab0d9482f28e3bc7f1c3cb7640b7edf63b60e10
Oghene Michael <zonalmeet@gmail.com>
2018-01-27 20:16:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase()) {
     return "I can't hear you!"
   }
+}
+
+function sayHiToGrandma(string) {
   if (string.toUpperCase()) {
     return "YES INDEED!"
   }
